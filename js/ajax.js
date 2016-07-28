9e4f74086f1078f35e6d581928ee486b924675b1
Nathan Heffley <nathanheffley@gmail.com>
2016-07-28 19:20:25 -0400

    Change from directly loading html to parsing JSON data files

--- js/ajax.js
+++ js/ajax.js
@@ -1,21 +1,27 @@
-function loadPage (loc) {
+$(document).on('click', '.content-link', function () {
+  var uri = $(this).attr('href').substring(3);
+  loadData(uri);
+});
+
+function loadData (loc) {
   if (loc == '') {
-    $.get('http://localhost:3000/pages/homepage.html', function (data, status) {
-      $("#content").html(data);
+    $.get('http://localhost:3000/pages/homepage.json', function (data, status) {
+      loadPage(data);
     });
   } else {
-    $.get('http://localhost:3000/pages/' + loc + '.html', function (data, status) {
-      $("#content").html(data);
+    $.get('http://localhost:3000/pages/' + loc + '.json', function (data, status) {
+      loadPage(data);
     });
   }
 }
 
-$(document).on('click', '.content-link', function () {
-  var loc = $(this).attr('href').substring(3);
-  loadPage(loc);
-});
-
-function handleLoad () {
-  var loc = window.location.hash.substring(2);
-  loadPage(loc)
+function loadPage (data) {
+  var page = "";
+  if (data.title) {
+    page += "<h1>" + data.title + "</h1>";
+  }
+  if (data.content) {
+    page += data.content;
+  }
+  $("#content").html(page);
 }
