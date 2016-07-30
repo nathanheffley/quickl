6b705ad2468d850d4265ae168cc23eacb87a9ae4
Nathan Heffley <nathanheffley@gmail.com>
2016-07-29 22:13:55 -0400

    Add featured images to post

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ var express = require('express');
 var mongoose = require("mongoose");
 mongoose.connect('mongodb://localhost/quickl');
 var postSchema = new mongoose.Schema({
-  slug: { type: String, required: true, unique: true},
-  title: { type: String, required: true},
-  content: { type: String, required: true}
-});
+  slug: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  image: { type: String, required: true }
+},
+{ timestamps: true});
 var Post = mongoose.model('Post', postSchema);
 
 var app = express();
@@ -28,7 +30,7 @@ app.get('/:slug', function (req, res) {
     if (err) {
       console.log(err);
     } else if (post) {
-      res.render('post', {postSlug: post.slug, postTitle: post.title, postContent: post.content});
+      res.render('post', {postSlug: post.slug, postTitle: post.title, postContent: post.content, postImage: post.image});
     } else {
       res.render('post', {postSlug: '404', postTitle: 'Post not found.', postContent: '<p>Sorry that I couldn\'t find what you\'re looking for :(</p>'});
     }
@@ -40,7 +42,7 @@ app.get('/:slug/amp', function (req, res) {
     if (err) {
       console.log(err);
     } else if (post) {
-      res.render(__dirname + '/amp/post', {postSlug: post.slug, postTitle: post.title, postContent: post.content});
+      res.render(__dirname + '/amp/post', {postSlug: post.slug, postTitle: post.title, postContent: post.content, postImage: post.image});
     } else {
       res.render(__dirname + '/amp/post', {postSlug: '404', postTitle: 'Post not found.', postContent: '<p>Sorry that I couldn\'t find what you\'re looking for :(</p>'});
     }
