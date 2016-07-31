3d4cf2529a5f2636c7f9e54cf6b58cd028be4162
Nathan Heffley <nathanheffley@gmail.com>
2016-07-30 22:13:33 -0400

    Refactor app routing code

--- app.js
+++ app.js
@@ -18,43 +18,58 @@ app.set('view engine', 'pug');
 app.use('/css', express.static(__dirname + '/css'));
 app.use('/js', express.static(__dirname + '/js'));
 
+/* Start Index Routing */
 app.get('/', function (req, res) {
-  Post.find({}).limit(10).sort({ createdAt: -1 }).exec(function (err, posts) {
-    if (err) throw err;
-    res.render('index', { posts: posts });
-  });
+  routeIndex(res, false);
 });
 
 app.get('/amp', function (req, res) {
-  Post.find({}).limit(10).sort({ createdAt: -1 }).exec(function (err, posts) {
-    if (err) throw err;
-    res.render(__dirname + '/amp/index', { posts: posts });
-  });
+  routeIndex(res, true);
 });
 
-app.get('/:slug', function (req, res) {
-  Post.findOne({'slug': req.params.slug}, function (err, post) {
-    if (err) {
-      console.log(err);
-    } else if (post) {
-      res.render('post', {postSlug: post.slug, postTitle: post.title, postAuthor: post.author, postContent: post.content, postImage: post.image, postCreated: post.createdAt, postUpdated: post.updatedAt});
+function routeIndex(res, amp) {
+  Post.find({}).limit(10).sort({createdAt: -1}).exec(function (err, posts) {
+    if (err) throw err;
+    if (amp) {
+      res.render(__dirname + '/amp/index', { posts: posts });
     } else {
-      res.render('post', {postSlug: '404', postTitle: 'Post not found.', postAuthor: 'not-found', postContent: '<p>Sorry that I couldn\'t find what you\'re looking for :(</p>', postImage: 'not-found', postCreated: 'not-found', postUpdated: 'not-found'});
+      res.render('index', { posts: posts });
     }
   });
+}
+/* End Index Routing */
+
+/* Start Post Routing */
+app.get('/:slug', function (req, res) {
+  routePost(req, res, false);
 });
 
 app.get('/:slug/amp', function (req, res) {
+  routePost(req, res, true);
+});
+
+function routePost(req, res, amp) {
   Post.findOne({'slug': req.params.slug}, function (err, post) {
     if (err) {
       console.log(err);
     } else if (post) {
-      res.render(__dirname + '/amp/post', {postSlug: post.slug, postTitle: post.title, postAuthor: post.author, postContent: post.content, postImage: post.image, postCreated: post.createdAt, postUpdated: post.updatedAt});
+      var postObj = {postSlug: post.slug, postTitle: post.title, postAuthor: post.author, postContent: post.content, postImage: post.image, postCreated: post.createdAt, postUpdated: post.updatedAt}
+      if (amp) {
+        res.render(__dirname + '/amp/post', postObj);
+      } else {
+        res.render('post', postObj);
+      }
     } else {
-      res.render(__dirname + '/amp/post', {postSlug: '404', postTitle: 'Post not found.', postAuthor: 'not-found', postContent: '<p>Sorry that I couldn\'t find what you\'re looking for :(</p>', postImage: 'not-found', postCreated: 'not-found', postUpdated: 'not-found'});
+      var _404Obj = {postSlug: '404', postTitle: 'Post not found.', postAuthor: 'not-found', postContent: '<p>Sorry that I couldn\'t find what you\'re looking for :(</p>', postImage: 'not-found', postCreated: 'not-found', postUpdated: 'not-found'}
+      if (amp) {
+        res.render(__dirname + '/amp/post', _404Obj);
+      } else {
+        res.render('post', _404Obj);
+      }
     }
   });
-});
+}
+/* End Post Routing */
 
 app.listen(port, function () {
   console.log('Quickl is listening on port ' + port)
