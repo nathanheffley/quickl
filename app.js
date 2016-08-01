ece7b5c6ab096e60ae3856d8ec09b1911905fdb3
Nathan Heffley <nathanheffley@gmail.com>
2016-08-01 12:27:39 -0400

    Redirect missing posts to homepage

--- app.js
+++ app.js
@@ -50,14 +50,14 @@ app.get('/:slug/amp', function (req, res) {
 
 function routePost(req, res, amp) {
   Post.findOne({'slug': req.params.slug}, function (err, post) {
-    var resObj = {postSlug: '404', postTitle: 'Post not found.', postAuthor: 'not-found', postContent: '<p>Sorry that I couldn\'t find what you\'re looking for :(</p>', postImage: 'not-found', postCreated: 'not-found', postUpdated: 'not-found'}; // Default 404 resonse object
-
-    // Check if a post was found
     if (err) {
       console.log(err);
     } else if (post) {
       // If a post was found and is not null, change the response object to the post
-      resObj = {postSlug: post.slug, postTitle: post.title, postAuthor: post.author, postContent: post.content, postImage: post.image, postCreated: post.createdAt, postUpdated: post.updatedAt}
+      var resObj = {postSlug: post.slug, postTitle: post.title, postAuthor: post.author, postContent: post.content, postImage: post.image, postCreated: post.createdAt, postUpdated: post.updatedAt}
+    } else {
+      // Redirect missing pages to the 404 page.
+      res.redirect('/'); // TODO: Redirect to 404 page.
     }
 
     // Finally, send the response object for the proper format
